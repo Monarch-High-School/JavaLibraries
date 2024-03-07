db6447f3f56920a9fbf7a99118a8d7082c69f7c5
Ben <142829070+benwillcutt@users.noreply.github.com>
2024-03-07 18:59:44 +0000

    Add Brightness tests and .gitignore file

--- ImageTester.java
+++ ImageTester.java
@@ -45,9 +45,16 @@ import java.util.Random;
         randomImageJPG.saveToFile("randomWithAlpha.jpg");
         
             // read in existing PNG image and bright all channels by 20 - save to testImageBright
-
+        Image brightenJPG = new Image(800, 600, Image.TYPE.RGB, Image.FORMAT.JPG);
+        pixels = brightenJPG.getPixels();
+        adjustBrightness(pixels, 20);
+        brightenJPG.saveToFile("testImageBright.jpg");
+        
             // read in existing JPG image and bright all channels by 20 - save to testImageBright
-
+        Image brightenPNG = new Image(800, 600, Image.TYPE.RGB, Image.FORMAT.PNG);
+        pixels = brightenPNG.getPixels();
+        adjustBrightness(pixels, 20);
+        brightenPNG.saveToFile("testImageBright.png");
             // read in existing PNG image and 0 out red - save to testImageNoRed
 
             // read in existing JPG image and 0 out red - save to testImageNoRed
@@ -88,5 +95,20 @@ import java.util.Random;
                 pixels[row][col].setAlpha(alpha);
             }
         }
+    }
+
+    /**
+     * Increases or decreases the brightness of a 2D pixel array by a specified amount.
+     * @param adjustment the amount to adjust each pixel's brightness by
+     */
+    private static void adjustBrightness(Pixel [][] pixels, int adjustment) {
+      for (int i = 0; i < pixels.length; i++) {
+        for (int j = 0; j < pixels[i].length; j++) {
+          pixels[i][j].setRed(pixels[i][j].getRed() + adjustment);
+          pixels[i][j].setGreen(pixels[i][j].getGreen() + adjustment);
+            pixels[i][j].setBlue(pixels[i][j].getBlue() + adjustment);
+            ;
+        }
+      }
     }
  }
