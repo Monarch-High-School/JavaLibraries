ec9c0e2a2b9d0cf4bb5098ab5911de6e8dd5d0cc
jonathoncihlar <jonathoncihlar@github.com>
2024-03-12 20:26:15 -0600

    Generate JavaDoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.bvsd","l":"Filter"},{"p":"org.bvsd","l":"Image.FORMAT"},{"p":"org.bvsd","l":"Image"},{"p":"org.bvsd","l":"Pixel"}];updateSearchResults();
