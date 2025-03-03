940a0f4dfc5d36843eae491940264ee472d4410d
jonathoncihlar <jonathoncihlar@github.com>
2025-03-03 12:54:39 -0700

    Update Filter.java to change from concrete class to an interface.

--- src/main/java/org/bvsd/Filter.java
+++ src/main/java/org/bvsd/Filter.java
@@ -1,55 +1,20 @@
 package org.bvsd;
 
 /**
- * Encapsulates data and methods for apply effects to an Image object. 
- * This class is intended to be subclassed with the apply method overridden.
- * Any additional parameters for the filter to do its job should be added. The constructor
- * should be overridden, but this constructor should also be called to set the Filter's name.
+ * The Filter interface specifies one method, apply(), which defines the behavior of 
+ * applying a specific effect to an image.
  * @author B. Willcutt
  * @author J. Cihlar
- * @version 1.0
+ * @version 2.0
  */
-public class Filter {
+interface Filter {
 
-  /**  The name of the filter **/
-  private String name;
 
   /**
-   * Constructor
-   * 
-   * @param n The name of the Filter
+   * Applies an effect to an Image object and returns a new Image.
+   * @param image The Image object to which to apply the effect
+   * @return Image A new image which contains 
    */
-  public Filter(String n) {
-    name = n;
-  }
+  public Image apply(Image image);
 
-  /**
-   * Applies the effect to an image. This method should be overridden in every subclass
-   * of the filter.
-   * 
-   * @param image The Image object to modify
-   */
-  public void apply(Image image) {
-    // apply a filter to the image
-  }
-
-  /** 
-   * Getter for name
-   * 
-   * @return The name set for this filter.
-  */
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * Produces a String that should be output before 
-   * the effect is applied. "Applying {name} filter"
-   * 
-   * @return The String "Applying {name} filter" where {name} is the name of the filter.
-   */
-  public String getApplyString() {
-    return "Applied " + name + " filter.";
-  }
-  
 }
